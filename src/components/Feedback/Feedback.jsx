908276211198dd37467bd8d5280c5caaf8efb82b
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-05-25 15:53:47 +0300

    new

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,26 +4,26 @@ import { StatsToRend } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
 const FeedbackStats = () => {
-  const [Good, setGood] = useState(0);
-  const [Neutral, setNeutral] = useState(0);
-  const [Bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const params = {
-    Good,
-    Neutral,
-    Bad,
+    good,
+    neutral,
+    bad,
   };
   const options = Object.keys(params);
 
   const onLeaveFeedback = type => {
     switch (type) {
-      case `Good`:
+      case `good`:
         setGood(PrevState => PrevState + 1);
         break;
-      case `Neutral`:
+      case `neutral`:
         setNeutral(PrevState => PrevState + 1);
         break;
-      case `Bad`:
+      case `bad`:
         setBad(PrevState => PrevState + 1);
         break;
 
@@ -32,35 +32,35 @@ const FeedbackStats = () => {
     }
   };
 
-  const countTotalFeedback = (Good, Neutral, Bad) => {
-    return Good + Neutral + Bad;
+  const countTotalFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = (
-    Good,
-    Bad,
-    Neutral,
+    good,
+    bad,
+    neutral,
     countTotalFeedback
   ) => {
-    const summ = (Good * 100) / countTotalFeedback(Good, Neutral, Bad);
+    const summ = (good * 100) / countTotalFeedback(good, neutral, bad);
     return Math.round(summ);
   };
 
   return (
     <>
       <OptionsToRend options={options} onLeaveFeedback={onLeaveFeedback} />
-      {countTotalFeedback(Good, Neutral, Bad) !== 0 ? (
+      {countTotalFeedback(good, neutral, bad) !== 0 ? (
         <StatsToRend
           positivePercentage={countPositiveFeedbackPercentage(
-            Good,
-            Bad,
-            Neutral,
+            good,
+            bad,
+            neutral,
             countTotalFeedback
           )}
-          good={Good}
-          bad={Bad}
-          neutral={Neutral}
-          total={countTotalFeedback(Good, Neutral, Bad)}
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          total={countTotalFeedback(good, neutral, bad)}
         />
       ) : (
         <Notification />
